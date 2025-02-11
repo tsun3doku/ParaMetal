07e3149002852108a7a19660aab1b761dcc0f5f9
tsun3doku <damian0710m@protonmail.com>
2025-02-11 02:26:22 -0700

    Added a heat source

--- HeatSpectra/shaders/compile.bat
+++ HeatSpectra/shaders/compile.bat
@@ -15,6 +15,7 @@ C:/VulkanSDK/1.3.283.0/Bin/glslc.exe lighting.frag -o composite_frag.spv
 
 C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_tetra.comp -o heat_tetra_comp.spv
 C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_surface.comp -o heat_surface_comp.spv
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_source.comp -o heat_source_comp.spv
 
 
 
