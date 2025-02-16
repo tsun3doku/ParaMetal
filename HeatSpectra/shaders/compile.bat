8c58be49cb1980d2bcf3ca1d7f1963be5a1a9b71
tsun3doku <damian0710m@protonmail.com>
2025-02-16 03:19:05 -0700

    Improved tetra heat transfer logic

--- HeatSpectra/shaders/compile.bat
+++ HeatSpectra/shaders/compile.bat
@@ -13,9 +13,9 @@ C:/VulkanSDK/1.3.283.0/Bin/glslc.exe lighting.frag -o lighting_frag.spv
 C:/VulkanSDK/1.3.283.0/Bin/glslc.exe lighting.vert -o composite_vert.spv
 C:/VulkanSDK/1.3.283.0/Bin/glslc.exe lighting.frag -o composite_frag.spv
 
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_tetra.comp -o heat_tetra_comp.spv
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_surface.comp -o heat_surface_comp.spv
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe heat_source.comp -o heat_source_comp.spv
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe --target-env=vulkan1.3 heat_tetra.comp -o heat_tetra_comp.spv
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe --target-env=vulkan1.3 heat_surface.comp -o heat_surface_comp.spv
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe --target-env=vulkan1.3 heat_source.comp -o heat_source_comp.spv
 
 
 
